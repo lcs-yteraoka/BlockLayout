31d5ad243a5400be0a03be4781c1de9f83fbb84b
Yahiro Teraoka <yteraoka@lcs.on.ca>
2026-02-07 09:56:50 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Yahiro Teraoka on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
