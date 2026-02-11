47b80bcf4ffd5e7e114a61ccccb92364c8f68598
Yahiro Teraoka <yteraoka@lcs.on.ca>
2026-02-11 09:53:21 -0500

    Completed a layout on my own using the skills I learned.

---
+++ BlockLayout/ContentViewThree.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewThree.swift
+//  BlockLayout
+//
+//  Created by Yahiro Teraoka on 2026-02-07.
+//
+
+import SwiftUI
+
+struct ContentViewThree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewThree()
+}
